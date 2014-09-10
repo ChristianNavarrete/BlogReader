499e7de3a37fbba4412e49fad0851b14c5b8335d
Christian Navarrete <idinkyy@gmail.com>
2014-09-10 14:36:14 -0700

    Initial Commit

---
+++ BlogReader/ABAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ABAppDelegate.h
+//  BlogReader
+//
+//  Created by HoodsDream on 9/10/14.
+//  Copyright (c) 2014 Asteroid Blues. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
